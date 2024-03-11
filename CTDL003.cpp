49ac631e6a8e79c80d20329fb3bb11ad9362e9ce
NguyenFrog <dinhnguyennguyen10082002@gmail.com>
2024-03-11 21:45:50 +0700

    Done

--- CTDL003.cpp
+++ CTDL003.cpp
@@ -1,38 +1,39 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
+
+#define faster() cin.tie(0)->sync_with_stdio(0)
+#define clean() cin.ignore(numeric_limits<streamsize>::max(), '\n');
+#define fi first
+#define se second
+#define pb push_back
+#define mod 1000000007
+#define ld long double
+#define ll long long 
+#define lli unsigned long long int
+#define For(i,a,b) for(ll i=a;i<b;i++)
+#define Forr(i,a,b) for(ll i=a;i>=b;i--)
+#define sortu(a) sort(a.begin(),a.end())
+#define sortv(a) sort(a.rbegin(),a.rend())
+#define pause() system("pause");
+
 using namespace std;
 string s;
-
-void numnext () {
-    int l=s.length(), check=1, k;
-    for (int i=l-1; i>=0; i--) 
-        if (s[i]=='0') {
-            check=0;
-            k=i;
-            break;
-        }
-    s[k]='1';
-    for (int i=k+1; i<=l; i++)
-        s[i]='0';   
-    if (check) {
-        for (int i=0; i<l; i++)
-            cout <<0;
-    }   else {
-            cout <<s;
-        }
-    cout <<endl;
-
-}
-
-int main () {
-    #ifndef ONLINE_JUDGE
-    freopen("input.txt","r",stdin);
-    freopen("output.txt","w",stdout);
-    #endif
-    int t;
-    cin >>t;
-    while (t--) {
-        cin >>s;
-        numnext ();
+void next(){
+    int i = (int)s.length() - 1;
+    while(i>=0 && s[i]=='1'){
+        s[i] = '0';
+        i--;
     }
-    return 0;
+    if(i>=0) s[i] = '1';
+    cout << s << endl;
+}
+void solve(){
+    int t; cin>>t;
+    while(t--){
+        cin>>s;
+        next();
+    }
+}
+int main(){
+    faster();
+    solve();
 }
