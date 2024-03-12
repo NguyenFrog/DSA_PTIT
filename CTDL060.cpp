ed78e0aaf5e5e5794b0151784bc789c2ac5e2da5
NguyenFrog <dinhnguyennguyen10082002@gmail.com>
2024-03-12 19:36:06 +0700

    Done test

---
+++ CTDL060.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+
+#define faster() cin.tie(0)->sync_with_stdio(0)
+#define clean() cin.ignore(numeric_limits<streamsize>::max(), '\n');
+#define fi first
+#define se second
+#define pb push_back
+#define mod 1000000007
+#define ld long double
+#define ll long long 
+#define lli unsigned long long int
+#define For(i,a,b) for(ll i=a;i<b;i++)
+#define Forr(i,a,b) for(ll i=a;i>=b;i--)
+#define sortu(a) sort(a.begin(),a.end())
+#define sortv(a) sort(a.rbegin(),a.rend())
+#define pause() system("pause");
+
+using namespace std;
+
+void solve(){
+    int t; cin>>t;
+    while(t--){
+    	int n; cin>>n;
+        int a[n];
+        for(int& x:a) cin>>x;
+        int count = 0;
+        for(auto x:a) if(!x) count++;
+        cout << count << endl;
+    }
+}
+int main(){
+    faster();
+    solve();
+}
