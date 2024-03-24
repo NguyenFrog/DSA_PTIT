f52878b60fb2ef1a32528a598f51349f9e60666e
NguyenFrog <dinhnguyennguyen10082002@gmail.com>
2024-03-25 01:25:13 +0700

    <testcase error/>

---
+++ CTDL083 - Đảo ngược thứ tự của các phần tử k đầu tiên .cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+
+#define faster() cin.tie(0)->sync_with_stdio(0)
+#define clean() cin.ignore(numeric_limits<streamsize>::max(), '\n');
+#define fi first
+#define se second
+#define pb push_back
+#define mod 1000000007
+#define ld long double
+#define ll long long 
+#define lli unsigned long long int
+#define For(i,a,b) for(ll i=a;i<b;i++)
+#define Forr(i,a,b) for(ll i=a;i>=b;i--)
+#define sortu(a) sort(a.begin(),a.end())
+#define sortv(a) sort(a.rbegin(),a.rend())
+#define pause() system("pause");
+ 
+using namespace std;
+
+void solve(){
+    int t; cin>>t;
+    while(t--){
+        int n,k; cin>>n>>k;
+        string ans = "";
+        queue<int> q;
+        stack<int> st;
+        For(i,0,n){
+            int x; cin>>x;  
+            q.push(x);        
+        }            
+        while(k--){
+            int top = q.front(); q.pop();
+            st.push(top);
+        }
+        while(!st.empty()){
+            ans += to_string(st.top()) + " ";
+            st.pop();
+        }
+        while(!q.empty()){
+            ans += to_string(q.front()) + " ";
+            q.pop();
+        }
+        ans.pop_back();
+        cout << ans << endl;
+    }
+}
+int main(){
+	faster();
+	solve();
+}
