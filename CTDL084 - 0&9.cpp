61045284bd02f4db3cb1dd0faf416c827d48ae54
NguyenFrog <dinhnguyennguyen10082002@gmail.com>
2024-03-24 15:40:01 +0700

    (chore) add new files

---
+++ CTDL084 - 0&9.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+
+#define faster() cin.tie(0)->sync_with_stdio(0)
+#define clean() cin.ignore(numeric_limits<streamsize>::max(), '\n');
+#define fi first
+#define se second
+#define pb push_back
+#define mod 1000000007
+#define ld long double
+#define ll long long 
+#define lli unsigned long long int
+#define For(i,a,b) for(ll i=a;i<b;i++)
+#define Forr(i,a,b) for(ll i=a;i>=b;i--)
+#define sortu(a) sort(a.begin(),a.end())
+#define sortv(a) sort(a.rbegin(),a.rend())
+#define pause() system("pause");
+ 
+using namespace std;
+
+ll ans[101];
+void build(){
+    queue<string> q;
+    vector<ll> v;
+    q.push("9");
+    v.pb(9);
+    while(!q.empty()){
+        string top = q.front(); q.pop();
+        if(top.length() > 15) break;
+        q.push(top + "0");
+        q.push(top + "9");
+        v.pb(stoll(top + "0"));
+        v.pb(stoll(top + "9"));
+    }
+    For(i,1,101){
+        for(auto x:v){
+            if(x%i==0 && !ans[i]){
+                ans[i] = x;
+                break;
+            }
+        }
+    }
+}
+void solve(){
+    int t; cin>>t;
+    build();
+    while(t--){
+        int n; cin>>n;
+        cout << ans[n] << endl;
+    }
+}
+int main(){
+	faster();
+	solve();
+}
